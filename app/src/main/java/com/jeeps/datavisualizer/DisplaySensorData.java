103c9add6d218be0d007b6be08cb7a7625459206
Jose Eduardo <pepe.96.jose@gmail.com>
2018-01-11 11:35:40 -0500

    Added Horizontal stacked bar chart to also display min an max temps of the week

--- app/src/main/java/com/jeeps/datavisualizer/DisplaySensorData.java
+++ app/src/main/java/com/jeeps/datavisualizer/DisplaySensorData.java
@@ -15,8 +15,11 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.db.chart.animation.Animation;
+import com.db.chart.model.BarSet;
 import com.db.chart.model.LineSet;
+import com.db.chart.renderer.AxisRenderer;
 import com.db.chart.view.HorizontalBarChartView;
+import com.db.chart.view.HorizontalStackBarChartView;
 import com.db.chart.view.LineChartView;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -46,7 +49,7 @@ public class DisplaySensorData extends AppCompatActivity {
     @BindView(R.id.week_temp_linechart)
     LineChartView mWeekTempChart;
     @BindView(R.id.week_temp_barchart)
-    HorizontalBarChartView mHorizontalBarChart;
+    HorizontalStackBarChartView mHorizontalBarChart;
     @BindView(R.id.current_temp_text)
     TextView mCurrentTempText;
     @BindView(R.id.thermometer_image)
@@ -59,6 +62,7 @@ public class DisplaySensorData extends AppCompatActivity {
 
     private final String[] weekLabels = {"M", "T", "W", "T", "F", "S", "S"};
     private final float[] weekDefault = {99,99,99,99,99,99,99};
+    private final float[] weekDefaultNeg = {-99,-99,-99,-99,-99,-99,-99};
 
     private boolean FIRST_LOAD = true;
 
@@ -111,9 +115,15 @@ public class DisplaySensorData extends AppCompatActivity {
         //Weekly temperature
         LineSet dataset = new LineSet(weekLabels, weekDefault);
         LineSet dataset2 = new LineSet(weekLabels, weekDefault);
+        BarSet barSet = new BarSet(weekLabels, weekDefaultNeg);
+        BarSet barSet2 = new BarSet(weekLabels, weekDefault);
 
+        //Datasets config
         mWeekTempChart.addData(dataset);
         mWeekTempChart.addData(dataset2);
+
+        mHorizontalBarChart.addData(barSet);
+        mHorizontalBarChart.addData(barSet2);
 
         /*dataset.setColor(Color.parseColor("#53c1bd"))
                 .setFill(Color.parseColor("#3d6c73"))
@@ -122,8 +132,19 @@ public class DisplaySensorData extends AppCompatActivity {
         dataset.setColor(Color.parseColor("#53c1bd"));
         dataset2.setColor(Color.parseColor("#5b5cbd"));
 
+        barSet.setColor(Color.parseColor("#53c1bd"));
+        barSet2.setColor(Color.parseColor("#5b5cbd"));
 
+        //Line chart config
         mWeekTempChart
+                .setStep(10)
+                .show(new Animation());
+
+        //Horizontal bar chart config
+        mHorizontalBarChart.setRoundCorners(50);
+        mHorizontalBarChart.setXLabels(AxisRenderer.LabelPosition.NONE);
+
+        mHorizontalBarChart
                 .setStep(10)
                 .show(new Animation());
     }
@@ -137,6 +158,7 @@ public class DisplaySensorData extends AppCompatActivity {
 
             private float[] mWeekTempMax;
             private float[] mWeekTempMin;
+            private float[] mWeekTempMinNegative;
 
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -164,29 +186,37 @@ public class DisplaySensorData extends AppCompatActivity {
                 //Weekly temperature
                 mWeekTempMin = listToArray(sensorData.getWeeklyTemperatureMin());
                 mWeekTempMax = listToArray(sensorData.getWeeklyTemperatureMax());
+                mWeekTempMinNegative = listToArray(sensorData.getWeeklyTemperatureMin());
+                for (int i = 0; i < mWeekTempMinNegative.length; i++)
+                    mWeekTempMinNegative[i] *= -1;
                 if (FIRST_LOAD) {
                     final Handler handler = new Handler();
                     handler.postDelayed(new Runnable() {
                         @Override
                         public void run() {
-                            //Min temp
-                            mWeekTempChart.dismissAllTooltips();
-                            mWeekTempChart.updateValues(0, mWeekTempMin);
-                            //Max temp
-                            mWeekTempChart.updateValues(1, mWeekTempMax);
-                            mWeekTempChart.notifyDataUpdate();
-
+                            updateCharts();
                         }
                     }, 2000);
                     FIRST_LOAD = false;
                 } else {
-                    //Min temp
-                    mWeekTempChart.dismissAllTooltips();
-                    mWeekTempChart.updateValues(0, mWeekTempMin);
-                    //Max temp
-                    mWeekTempChart.updateValues(1, mWeekTempMax);
-                    mWeekTempChart.notifyDataUpdate();
+                    updateCharts();
+
                 }
+            }
+
+            private void updateCharts() {
+                //Min temp
+                mWeekTempChart.dismissAllTooltips();
+                mWeekTempChart.updateValues(0, mWeekTempMin);
+                //Max temp
+                mWeekTempChart.updateValues(1, mWeekTempMax);
+                mWeekTempChart.notifyDataUpdate();
+
+                //Horizontal bar chart
+                mHorizontalBarChart.dismissAllTooltips();
+                mHorizontalBarChart.updateValues(0, mWeekTempMinNegative);
+                mHorizontalBarChart.updateValues(1, mWeekTempMax);
+                mHorizontalBarChart.notifyDataUpdate();
             }
 
             @Override
