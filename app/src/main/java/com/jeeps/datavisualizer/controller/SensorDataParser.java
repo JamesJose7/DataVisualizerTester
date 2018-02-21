242d06cfc4ff84d3f5b7f5ea7e7babe688740d2c
Jose Eduardo <pepe.96.jose@gmail.com>
2018-02-21 11:50:52 -0500

    Getting min and max temp from each day

--- app/src/main/java/com/jeeps/datavisualizer/controller/SensorDataParser.java
+++ app/src/main/java/com/jeeps/datavisualizer/controller/SensorDataParser.java
@@ -15,7 +15,8 @@ import java.util.Arrays;
 public class SensorDataParser {
     private SensorData mSensorData;
 
-    private Float[] weeklyTemp = new Float[7];
+    private Float[] weeklyTempMin = new Float[7];
+    private Float[] weeklyTempMax = new Float[7];
 
     public SensorDataParser(SensorData sensorData) {
         mSensorData = sensorData;
@@ -43,8 +44,11 @@ public class SensorDataParser {
 
         //Set value to object
         mSensorData.setTemperature(temperature);
+        //Get min and max
+        float[] minMax = getMinMax(data);
         //Weekly temp
-        weeklyTemp[0] = (float) temperature;
+        weeklyTempMin[0] = minMax[0];
+        weeklyTempMax[0] = minMax[1];
     }
 
     public void parseWeeklyTemp(String jsonData, int dateCounter) throws JSONException {
@@ -56,13 +60,32 @@ public class SensorDataParser {
         float temperature = Float.parseFloat(temperatureValue);
 
         //Set weekly temp
-        weeklyTemp[dateCounter] = temperature;
+        //weeklyTemp[dateCounter] = temperature;
+        //Get min and max
+        float[] minMax = getMinMax(data);
+        //Weekly temp
+        weeklyTempMin[dateCounter] = minMax[0];
+        weeklyTempMax[dateCounter] = minMax[1];
         //Set value to object
-        mSensorData.setWeeklyTemperatureMax(Arrays.asList(weeklyTemp));
-        mSensorData.setWeeklyTemperatureMin(Arrays.asList(weeklyTemp));
+        mSensorData.setWeeklyTemperatureMax(Arrays.asList(weeklyTempMax));
+        mSensorData.setWeeklyTemperatureMin(Arrays.asList(weeklyTempMin));
     }
 
     public SensorData getSensorData() {
         return mSensorData;
+    }
+
+    private float[] getMinMax(JSONArray array) throws JSONException {
+        //Get first value
+        float min = Float.parseFloat(array.getJSONObject(0).getString("valor"));
+        float max = Float.parseFloat(array.getJSONObject(0).getString("valor"));
+        for (int i = 1; i < array.length(); i++) {
+            float currentValue = Float.parseFloat(array.getJSONObject(i).getString("valor"));
+            if (currentValue < min)
+                min = currentValue;
+            if (currentValue > max)
+                max = currentValue;
+        }
+        return new float[]{min, max};
     }
 }
