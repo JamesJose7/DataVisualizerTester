c56e0f53c506bf46b848f36825d282fd9ec7ce40
Jose Eduardo <pepe.96.jose@gmail.com>
2018-03-31 22:28:19 -0500

    Removed logic to calculate values, now it just uses the data retrieved from the API

--- app/src/main/java/com/jeeps/datavisualizer/controller/SensorDataParser.java
+++ app/src/main/java/com/jeeps/datavisualizer/controller/SensorDataParser.java
@@ -7,9 +7,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 /**
@@ -18,9 +20,6 @@ import java.util.Locale;
 
 public class SensorDataParser {
     private SensorData mSensorData;
-
-    private Float[] weeklyTempMin = new Float[7];
-    private Float[] weeklyTempMax = new Float[7];
 
     public SensorDataParser(SensorData sensorData) {
         mSensorData = sensorData;
@@ -48,78 +47,52 @@ public class SensorDataParser {
 
         //Set value to object
         mSensorData.setTemperature(temperature);
-        //Get min and max
-        float[] minMax = getMinMax(data);
-        //Weekly temp
-        weeklyTempMin[0] = minMax[0];
-        weeklyTempMax[0] = minMax[1];
-
-        //Get hourly temperature
-        SimpleDateFormat hourFormatter = new SimpleDateFormat("H", Locale.US);
-        //Get last 7 hours
-        int[] lastHours = new int[7];
-        float[] hourlyTemperature = new float[7];
-        for (int i = 0; i < lastHours.length; i++) {
-            String hour = hourFormatter.format(getPreviousHourDate(i));
-            lastHours[i] = Integer.parseInt(hour);
-        }
-        //Get the temperature of each hour
-        for (int i = 0; i < data.length(); i++) {
-            JSONObject jsonObject = data.getJSONObject(i);
-            //Get current temperature
-            Float hourTemperature = Float.parseFloat(jsonObject.getString("valor"));
-            //Get saved hour
-            String time = jsonObject.getString("hora");
-            int hour = Integer.parseInt(time.split(":")[0]);
-
-            //Save each temperature on it's according hour
-            for (int j = 0; j < hourlyTemperature.length; j++) {
-                if (hour == lastHours[j])
-                    hourlyTemperature[j] = hourTemperature;
-            }
-        }
-        Float[] newHourlyTemp = new Float[hourlyTemperature.length];
-        for (int i = 0; i < hourlyTemperature.length; i++)
-            newHourlyTemp[i] = hourlyTemperature[i];
-        mSensorData.setHourlyTemperature(Arrays.asList(newHourlyTemp));
     }
 
-    public void parseWeeklyTemp(String jsonData, int dateCounter) throws JSONException {
+    public void parseHourlyTemperature(String jsonData) throws JSONException {
         JSONObject mainObject = new JSONObject(jsonData);
         //Get data array
         JSONArray data = mainObject.getJSONArray("data");
-        //Get temperature from first item
-        String temperatureValue = data.getJSONObject(0).getString("valor");
-        float temperature = Float.parseFloat(temperatureValue);
 
-        //Set weekly temp
-        //weeklyTemp[dateCounter] = temperature;
-        //Get min and max
-        float[] minMax = getMinMax(data);
-        //Weekly temp
-        weeklyTempMin[dateCounter] = minMax[0];
-        weeklyTempMax[dateCounter] = minMax[1];
+        List<Float> maxs = new ArrayList<>();
+        List<Float> mins = new ArrayList<>();
+
+        for (int i = 0; i < data.length(); i++) {
+            JSONObject jsonObject = data.getJSONObject(i);
+            String valueMax = jsonObject.getString("value_max");
+            String valueMin = jsonObject.getString("value_min");
+
+            maxs.add(Float.parseFloat(valueMax));
+            mins.add(Float.parseFloat(valueMin));
+        }
+
+        mSensorData.setHourlyTemperature(maxs);
+    }
+
+    public void parseWeeklyTemp(String jsonData) throws JSONException {
+        JSONObject mainObject = new JSONObject(jsonData);
+        //Get data array
+        JSONArray data = mainObject.getJSONArray("data");
+
+        List<Float> mins = new ArrayList<>();
+        List<Float> maxs = new ArrayList<>();
+
+        for (int i = 0; i < data.length(); i++) {
+            JSONObject currentObject = data.getJSONObject(i);
+            //Get min and max
+            String max = currentObject.getString("value_max");
+            String min = currentObject.getString("value_min");
+
+            maxs.add(Float.parseFloat(max));
+            mins.add(Float.parseFloat(min));
+        }
         //Set value to object
-        mSensorData.setWeeklyTemperatureMax(Arrays.asList(weeklyTempMax));
-        mSensorData.setWeeklyTemperatureMin(Arrays.asList(weeklyTempMin));
+        mSensorData.setWeeklyTemperatureMax(maxs);
+        mSensorData.setWeeklyTemperatureMin(mins);
     }
 
     public SensorData getSensorData() {
         return mSensorData;
-    }
-
-    private float[] getMinMax(JSONArray array) throws JSONException {
-        //Get first value
-        float min = Float.parseFloat(array.getJSONObject(0).getString("valor"));
-        float max = Float.parseFloat(array.getJSONObject(0).getString("valor"));
-        for (int i = 1; i < array.length(); i++) {
-            float currentValue = Float.parseFloat(array.getJSONObject(i).getString("valor"));
-            if (currentValue < min)
-                min = currentValue;
-            if (currentValue > max)
-                max = currentValue;
-        }
-        return new float[]{min, max};
     }
 
     public static Date getPreviousDayDate(int previousDays) {
