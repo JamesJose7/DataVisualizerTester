898c0f0b2b5c5d529141e9b0aebe0194d2dac993
Jose Eduardo <pepe.96.jose@gmail.com>
2018-03-31 22:26:41 -0500

    Added new methods for new api callbacks

--- app/src/main/java/com/jeeps/datavisualizer/controller/ApiBuilder.java
+++ app/src/main/java/com/jeeps/datavisualizer/controller/ApiBuilder.java
@@ -12,6 +12,10 @@ public class ApiBuilder {
 
     private static final String BASE_SENSOR_URL = "http://carbono.utpl.edu.ec:8080/smartlandiotv2/webresources/entidades.datos/getdatatable?ak=";
 
+    private static final String SENSOR_DAY_RESUME_URL = "http://carbono.utpl.edu.ec:8080/smartlandiotv2/webresources/entidades.datos/sensordayresume?ak=";
+    private static final String RESUME_BY_RANGE_DAYS_URL = "http://carbono.utpl.edu.ec:8080/smartlandiotv2/webresources/entidades.datos/resumebyrangedays?ak=";
+
+    @Deprecated
     public static String buildSensorUrl(String nodeName, String sensorName, String date) {
         StringBuilder builder = new StringBuilder();
         //Base url
@@ -26,6 +30,45 @@ public class ApiBuilder {
         //Date
         builder.append("&fecha=");
         builder.append(date);
+
+        return builder.toString();
+    }
+
+    public static String buildValuesByHourUrl(String nodeName, String sensorName, String date) {
+        StringBuilder builder = new StringBuilder();
+        //Base url
+        builder.append(SENSOR_DAY_RESUME_URL);
+        builder.append(API_KEY);
+        //Node name
+        builder.append("&node=");
+        builder.append(nodeName);
+        //Sensor name
+        builder.append("&sensor=");
+        builder.append(sensorName);
+        //Date
+        builder.append("&fecha=");
+        builder.append(date);
+
+        return builder.toString();
+    }
+
+    public static String buildValuesByDateRangeUrl(String nodeName, String sensorName, String date1, String date2) {
+        StringBuilder builder = new StringBuilder();
+        //Base url
+        builder.append(RESUME_BY_RANGE_DAYS_URL);
+        builder.append(API_KEY);
+        //Node name
+        builder.append("&node=");
+        builder.append(nodeName);
+        //Sensor name
+        builder.append("&sensor=");
+        builder.append(sensorName);
+        //Date 1
+        builder.append("&fecha1=");
+        builder.append(date1);
+        //Date 2
+        builder.append("&fecha2=");
+        builder.append(date2);
 
         return builder.toString();
     }
